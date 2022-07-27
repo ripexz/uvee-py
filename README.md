1c6f96976025eb3384d4b3a8260ab6d6ab1a1429
ripexz <mail@ripexz.com>
2022-07-27 19:15:01 +0200

    remove some debug stuff, add info and helper files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # uvee-py
+
 A wearable UV Index indicator
+
+**Current setup:**
+- [Adafruit Qt Py M0 SAMD21 Dev Board](https://www.adafruit.com/product/4600)
+- [Adafruit LTR390 UV Light Sensor](https://www.adafruit.com/product/4831)
+- [Monochrome 0.91" 128x32 I2C OLED Display](https://www.adafruit.com/product/4440)
+
+Everything's currently connected using JST SH cables leveraging STEMMA QT.
--- uvee-py.ino
+++ uvee-py.ino
@@ -54,33 +54,8 @@ void setup() {
   showText("Found LTR sensor!", 1);
 
   ltr.setMode(LTR390_MODE_UVS);
-  if (ltr.getMode() == LTR390_MODE_ALS) {
-    showText("In ALS mode", 1);
-  } else {
-    showText("In UVS mode", 1);
-  }
-
   ltr.setGain(LTR390_GAIN_18);
-  Serial.print("Gain : ");
-  switch (ltr.getGain()) {
-    case LTR390_GAIN_1: Serial.println(1); break;
-    case LTR390_GAIN_3: Serial.println(3); break;
-    case LTR390_GAIN_6: Serial.println(6); break;
-    case LTR390_GAIN_9: Serial.println(9); break;
-    case LTR390_GAIN_18: Serial.println(18); break;
-  }
-
   ltr.setResolution(LTR390_RESOLUTION_20BIT);
-  Serial.print("Resolution : ");
-  switch (ltr.getResolution()) {
-    case LTR390_RESOLUTION_13BIT: Serial.println(13); break;
-    case LTR390_RESOLUTION_16BIT: Serial.println(16); break;
-    case LTR390_RESOLUTION_17BIT: Serial.println(17); break;
-    case LTR390_RESOLUTION_18BIT: Serial.println(18); break;
-    case LTR390_RESOLUTION_19BIT: Serial.println(19); break;
-    case LTR390_RESOLUTION_20BIT: Serial.println(20); break;
-  }
-
   ltr.setThresholds(100, 1000);
   ltr.configInterrupt(true, LTR390_MODE_UVS);
 }
