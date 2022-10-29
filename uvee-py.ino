035a9116f62b5659db5588b9deb6ac024c3af0f0
ripexz <mail@ripexz.com>
2022-10-29 12:03:55 +0200

    test with uno+guva+small screen

--- uvee-py.ino
+++ uvee-py.ino
@@ -1,203 +1,77 @@
-#include <Adafruit_NeoPixel.h>
-#include <Adafruit_LTR390.h>
-#include <SPI.h>
+// Display – https://pmdway.com/collections/oled-displays/products/0-49-64-x-32-white-graphic-oled-i2c
+// Guide – https://pmdway.com/blogs/product-guides-for-arduino/tutorial-using-the-0-49-64-x-32-graphic-i2c-oled-display-with-arduino
+
+#include <Arduino.h>
+#include <U8g2lib.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datashebet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-#define UV_SENSITIVITY 2300
-#define WINDOW_FACTOR 1
-
-Adafruit_LTR390 ltr = Adafruit_LTR390();
-
-// additional data for UV sensor
-float gain_factor[5] = {1, 3, 6, 9, 18};
-float res_factor[6] = {4, 2, 1, 0.5, 0.25, 0.03125};
-
-// set up built-in neopixel on PIN_NEOPIXEL
-Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
-
-// battery level
-int batt_pin = A2; // Qt pPy BFF
-bool battBlinkOn = true;
-
-// debug
-bool batteryDebug = false;
+int uvPin = A0;
 
 void setup() {
-  Serial.begin(115200);
-
-  pinMode(batt_pin, INPUT);
-
-  delay(200); // delay before display init
-
-  pixels.begin();
-  pixels.setBrightness(25);
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (1) showPixelErrorLoop();
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  if (!ltr.begin()) {
-    showText("Couldn't find LTR sensor!", 1);
-    while (1) showPixelErrorLoop();
-  }
-  Serial.println("Found LTR sensor!");
-  showText("Found LTR sensor!", 1);
-
-  ltr.setMode(LTR390_MODE_UVS);
-  ltr.setGain(LTR390_GAIN_18);
-  ltr.setResolution(LTR390_RESOLUTION_20BIT);
-  ltr.setThresholds(100, 1000);
-  ltr.configInterrupt(true, LTR390_MODE_UVS);
+//  Serial.begin(9600);
+  u8g2.begin();
 }
 
+// fonts https://github.com/olikraus/u8g2/wiki/fntlistall#4-pixel-height
+
 void loop() {
-  float voltage = analogRead(batt_pin);
-  voltage *= 2.0; // BFF outputs divided by 2
-  voltage *= 3.3; // reference voltage
-  voltage /= 1024; // map to voltage range
+  float uvValue = analogRead(uvPin);
+//  Serial.println(uvValue);
+  uvValue *= 5; // Uno reference voltage
+  uvValue /= 1023.0; // map to voltage range
+  uvValue *= 10.0; // calculate UVI based on first stage of LM358 opamp
+  uvValue /= 6.1; // negate 2nd stage of LM358 opamp
 
-  if (batteryDebug) {
-    Serial.print("Voltage: ");
-    Serial.println(voltage);
-  }
-
-  drawBatteryIndicator(voltage);
-
-  if (ltr.newDataAvailable()) {
-    uint32_t raw = ltr.readUVS();
-    uint8_t _gain = (uint8_t)ltr.getGain();
-    uint8_t _resolution = (uint8_t)(ltr.getResolution());
-
-    Serial.print("UV data: ");
-    Serial.println(raw);
-
-    float uvi = (float)(raw) / ((gain_factor[_gain] / gain_factor[LTR390_GAIN_18]) * (res_factor[_resolution] / res_factor[LTR390_RESOLUTION_20BIT]) * (float)(UV_SENSITIVITY)) * (float)(WINDOW_FACTOR);
-
-    String text = "UVI: ";
-    text += uvi;
-
-    if (batteryDebug) {
-      text += "\nV: ";
-      text += voltage;
-    } else {
-      text += "\n" + getUVRatingText(uvi);
-    }
-
-    showText(text, 2);
-  }
-
-
+  drawScreen(uvValue, getUVRatingText(uvValue));
   delay(100);
+
+//  drawScreen(1.32, "Low");
+//  delay(1000);
+//
+//  drawScreen(4.20, "Mod.");
+//  delay(1000);
+//
+//  drawScreen(7.6912, "High");
+//  delay(1000);
+//
+//  drawScreen(9.00, "V.High");
+//  delay(1000);
+//
+//  drawScreen(11.32, "Xtreme");
+//  delay(1000);
+}
+
+void drawScreen(float value, String text) {
+  u8g2.clearBuffer();
+  drawVal(value);
+  u8g2.setFont(u8g2_font_t0_15_tr);
+  u8g2.drawStr(0, 28, text.c_str());
+  drawBatt();
+  u8g2.sendBuffer();
+}
+
+void drawVal(float value) {
+  u8g2.setFont(u8g2_font_t0_22_tf);
+  u8g2.setCursor(0, 14);
+  u8g2.print(value);
+}
+
+void drawBatt() {
+  u8g2.drawBox(58, 1, 4, 2);
+  u8g2.drawBox(56, 3, 8, 28);
 }
 
 String getUVRatingText(float uvi) {
   if (uvi < 3) {
     return "Low";
   } else if (uvi < 6) {
-    return "Moderate";
+    return "Mod";
   } else if (uvi < 8) {
     return "High";
   } else if (uvi < 11) {
-    return "Very High";
+    return "V. High";
   }
-  return "Extreme";
-}
-
-void showText(String text, int textSize) {
-  // display.clearDisplay();
-  display.fillRect(0, 0, display.width() - 8, display.height(), SSD1306_BLACK); // clear all but battery side
-  display.setTextSize(textSize);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.cp437(true);
-
-  display.println(text);
-
-  display.display();
-  delay(100);
-}
-
-void showPixelErrorLoop() {
-  showPixelColor(255, 0, 0);
-  delay(200);
-  showPixelColor(255, 0, 255);
-  delay(100);
-  pixels.clear();
-  pixels.show();
-  delay(500);
-}
-
-void showPixelColor(int r, int g, int b) {
-  pixels.setPixelColor(0, pixels.Color(r, g, b));
-  pixels.show();
-  delay(1000);
-}
-
-void drawBatteryIndicator(float voltage) {
-  // display battery level or charging
-  int padding = 1;
-  int graphicHeight = 2;
-  int battHeight = display.height() - graphicHeight - padding * 2;
-  int battWidth = 8;
-
-  // clear working area (full height)
-  display.fillRect(display.width()-battWidth, 0, battWidth, display.height(), SSD1306_BLACK);
-  // draw bettery terminal graphic
-  display.fillRect(display.width()-(battWidth/4*3), padding, battWidth/2, graphicHeight, SSD1306_WHITE);
-
-  // charging
-  if (voltage > 4.1) {
-    if (battBlinkOn) {
-      display.fillRect(display.width()-battWidth, padding + graphicHeight, battWidth, battHeight, SSD1306_WHITE);
-    } else {
-      display.drawRect(display.width()-battWidth, padding + graphicHeight, battWidth, battHeight, SSD1306_WHITE);
-    }
-    battBlinkOn = !battBlinkOn;
-    display.display();
-    delay(500);
-    return;
-  }
-
-  // on battery power, show level:
-  float cutoff = 3.2; // 0% level
-  int maxFill = battHeight - 2; // leave space for rect line
-  float battLevel = (voltage - cutoff) / (4.0 - cutoff) * (maxFill);
-  Serial.println(battLevel);
-  int fillLevel = round(battLevel);
-
-  display.drawRect(display.width()-battWidth, padding + graphicHeight, battWidth, battHeight, SSD1306_WHITE);
-
-  if (fillLevel > maxFill) { // just in case
-    fillLevel = maxFill;
-    Serial.print("fillLevel went above maxFill: ");
-    Serial.println(fillLevel);
-  }
-
-  display.fillRect(
-    display.width() - battWidth + 1,
-    display.height() - padding - 1 - fillLevel,
-    battWidth - 2,
-    fillLevel,
-    SSD1306_WHITE);
-
-  display.display();
+  return "Xtreme";
 }
